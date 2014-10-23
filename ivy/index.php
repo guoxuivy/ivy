837501258978f4b47e3ca3359af843abf10eff53
guoxuivy <guoxuivy@gmail.com>
2014-10-24 07:59:39 +0800

    Revert "t"
    This reverts commit 18537ed8c94ae61cf5f0f1b4f8881bb616f5e60b.

--- ivy/index.php
+++ ivy/index.php
@@ -3,6 +3,5 @@ $ivy=dirname(__FILE__).'/framework/Ivy.php';
 
 defined('IVY_DEBUG') or define('IVY_DEBUG',true);
 //error_reporting(E_ALL & ~E_NOTICE);
-//ttt
 require_once($ivy);
 $app = Ivy::createApplication()->run();
