65f0e25d94bc3a5932157d28baedc634ec8170a7
Sean Guo <guoxuivy@gmail.com>
2015-04-08 16:44:50 +0800

    ok

--- ivy/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-$ivy=dirname(__FILE__).'/framework/Ivy.php';
-
-defined('IVY_DEBUG') or define('IVY_DEBUG',true);
-//error_reporting(E_ALL & ~E_NOTICE);
-require_once($ivy);
-$app = Ivy::createApplication()->run();
